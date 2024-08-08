a99aa72fea118483f0f973075a23427ea9d4ae87
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-08-08 12:59:57 +0300

    sensor

--- GNS/filling_station/views.py
+++ GNS/filling_station/views.py
@@ -31,6 +31,9 @@ def reader_info(request, reader='1'):
     balloons = Balloon.objects.order_by('-id').filter(status=STATUS_LIST[reader])
     current_quantity_by_sensor = BalloonAmount.objects.filter(reader_id=reader, change_date=current_date)
     previous_quantity_by_sensor = BalloonAmount.objects.filter(reader_id=reader, change_date=previous_date)
+
+    print(current_quantity_by_sensor,previous_quantity_by_sensor,sep="-----")
+
     paginator = Paginator(balloons, 15)
     page_num = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_num)
@@ -41,7 +44,7 @@ def reader_info(request, reader='1'):
         format_required_date = datetime.strptime(required_date, '%d.%m.%Y')
 
         dataset = BalloonResources().export(Balloon.objects.order_by('-id').filter(state=STATUS_LIST[reader],
-                                                                                   creation_date=format_required_date))
+                                                                                   change_date=format_required_date))
         response = HttpResponse(dataset.xls, content_type='xls')
         response[
             'Content-Disposition'] = f'attachment; filename="RFID_1_{datetime.strftime(format_required_date, '%Y.%m.%d')}.xls"'
@@ -50,11 +53,18 @@ def reader_info(request, reader='1'):
         date_process = GetBalloonsAmount()
         format_required_date = datetime.today()
 
-    last_date_amount = len(balloons.filter(creation_date=datetime.today()))
-    previous_date_amount = len(balloons.filter(creation_date=datetime.today() - timedelta(days=1)))
-    required_date_amount = len(balloons.filter(creation_date=format_required_date))
+    last_date_amount = len(balloons.filter(change_date=datetime.today()))
+    previous_date_amount = len(balloons.filter(change_date=datetime.today() - timedelta(days=1)))
+    required_date_amount = len(balloons.filter(change_date=format_required_date))
 
     view_required_data = datetime.strftime(format_required_date, '%d.%m.%Y')
+
+    current_quantity_by_sensor_item = previous_quantity_by_sensor_item = 0
+    for amount in current_quantity_by_sensor:
+        current_quantity_by_sensor_item = amount.amount_of_balloons
+    for amount in previous_quantity_by_sensor:
+        previous_quantity_by_sensor_item = amount.amount_of_balloons
+    
 
     return render(request, "balloons_table.html", {
         "page_obj": page_obj,
@@ -62,6 +72,6 @@ def reader_info(request, reader='1'):
         'previous_balloons_amount': previous_date_amount,
         'required_date_amount': required_date_amount,
         'format_required_date': view_required_data,
-        'current_quantity_by_sensor': current_quantity_by_sensor,
-        'previous_quantity_by_sensor': previous_quantity_by_sensor,
+        'current_quantity_by_sensor': current_quantity_by_sensor_item,
+        'previous_quantity_by_sensor': previous_quantity_by_sensor_item,
         'form': date_process})
