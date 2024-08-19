cc951bc1820a0d8a8c995f6ebfbc41ebed03d16b
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-08-19 15:37:50 +0300

    miriada

--- rfid app/miriada.py
+++ rfid app/miriada.py
@@ -3,18 +3,21 @@ import requests
 
 def get_balloon_by_nfc_tag(nfc_tag):
     url = f'https://publicapi-vitebsk.cloud.gas.by/getballoonbynfctag?nfctag={nfc_tag}&realm=brestoblgas'
-    response = requests.get(url, timeout=1)
-    if response.status_code == 200:
-        try:
+
+    try:
+        response = requests.get(url, timeout=1)
+        if response.status_code == 200:
             if response.json()['status'] == "Ok":
                 return True, response.json()['List']
 
             else:  # ['status'] == "Error"
                 return False, response.json()['error']
-        except KeyError:
-            return False, {"status": "no valid response"}
-    else:
-        return False, {"status": response.status_code}
+        else:
+            return False, {"status": response.status_code}
+    except KeyError:
+        return False, {"status": "no valid response"}
+    except:
+        return False, {"status": "no valid response"}
 
 
 def search_balloon_by_nfc_tag(nfc_tag):
