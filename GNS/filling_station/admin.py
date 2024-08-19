9d797cdbb360caf0ea37ee6d361daf7c6a203d90
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-08-19 13:38:17 +0300

    admin register add

--- GNS/filling_station/admin.py
+++ GNS/filling_station/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Balloon, Truck
+from .models import (Balloon, Truck, Trailer, RailwayTanks, TTN, ShippingBatchBalloons, ReceivingBatchBalloons,
+                     ShippingBatchRailway)
 from import_export import resources
 
 
@@ -18,6 +19,44 @@ class BalloonAdmin(admin.ModelAdmin):
 
 @admin.register(Truck)
 class TruckAdmin(admin.ModelAdmin):
-    list_display = ['id', 'registration_number', 'type', 'max_capacity_cylinders_by_type',
+    list_display = ['id', 'car_brand', 'registration_number', 'type', 'max_capacity_cylinders_by_type',
                     'max_weight_of_transported_cylinders', 'max_mass_of_transported_gas', 'empty_weight',
-                    'full_weight']
+                    'full_weight', 'is_on_station', 'entry_date', 'entry_time', 'departure_date', 'departure_time']
+
+
+@admin.register(Trailer)
+class TrailerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'trailer_brand', 'registration_number', 'type', 'max_capacity_cylinders_by_type',
+                    'max_weight_of_transported_cylinders', 'max_mass_of_transported_gas', 'empty_weight',
+                    'full_weight', 'is_on_station']
+
+
+@admin.register(RailwayTanks)
+class RailwayTanksAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number', 'empty_weight', 'full_weight', 'is_on_station', 'entry_date', 'entry_time',
+                    'departure_date', 'departure_time']
+
+
+@admin.register(TTN)
+class TTNAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number', 'contract', 'name_of_supplier', 'gas_amount', 'balloons_amount', 'date']
+
+
+@admin.register(ShippingBatchBalloons)
+class ShippingBatchBalloonsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'begin_date', 'begin_time', 'end_date', 'end_time', 'truck', 'trailer',
+                    'amount_of_rfid', 'amount_of_5_liters', 'amount_of_20_liters', 'amount_of_50_liters', 'gas_amount',
+                    'balloons_list', 'is_active', 'ttn']
+
+
+@admin.register(ReceivingBatchBalloons)
+class ReceivingBatchBalloonsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'begin_date', 'begin_time', 'end_date', 'end_time', 'truck', 'trailer',
+                    'amount_of_rfid', 'amount_of_5_liters', 'amount_of_20_liters', 'amount_of_50_liters', 'gas_amount',
+                    'balloons_list', 'is_active', 'ttn']
+
+
+@admin.register(ShippingBatchRailway)
+class ShippingBatchRailwayAdmin(admin.ModelAdmin):
+    list_display = ['id', 'begin_date', 'begin_time', 'end_date', 'end_time', 'gas_amount', 'railway_tanks_list',
+                    'is_active', 'ttn']
--- GNS/filling_station/models.py
+++ GNS/filling_station/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 
 class Balloon(models.Model):
@@ -31,6 +32,7 @@ class Balloon(models.Model):
 
 
 class Truck(models.Model):
+    car_brand = models.CharField(null=True, blank=False, max_length=20, verbose_name="Марка авто")
     registration_number = models.CharField(blank=False, max_length=10, verbose_name="Регистрационный знак")
     type = models.CharField(blank=False, max_length=50, verbose_name="Тип")
     max_capacity_cylinders_by_type = models.IntegerField(null=True, blank=True,
@@ -41,6 +43,11 @@ class Truck(models.Model):
                                                       verbose_name="Максимальная масса перевозимого газа")
     empty_weight = models.IntegerField(null=True, blank=True, verbose_name="Вес пустого т/с")
     full_weight = models.IntegerField(null=True, blank=True, verbose_name="Вес полного т/с")
+    is_on_station = models.BooleanField(null=True, blank=True, verbose_name="Находится на станции")
+    entry_date = models.DateField(null=True, blank=True, verbose_name="Дата въезда")
+    entry_time = models.TimeField(null=True, blank=True, verbose_name="Время въезда")
+    departure_date = models.DateField(null=True, blank=True, verbose_name="Дата выезда")
+    departure_time = models.TimeField(null=True, blank=True, verbose_name="Время выезда")
 
     def __str__(self):
         return self.registration_number
@@ -50,8 +57,133 @@ class Truck(models.Model):
         verbose_name_plural = "Грузовики"
 
 
+class Trailer(models.Model):
+    trailer_brand = models.CharField(null=True, blank=False, max_length=20, verbose_name="Марка прицепа")
+    registration_number = models.CharField(blank=False, max_length=10, verbose_name="Регистрационный знак")
+    type = models.CharField(blank=False, max_length=50, verbose_name="Тип")
+    max_capacity_cylinders_by_type = models.IntegerField(null=True, blank=True,
+                                                         verbose_name="Максимальная вместимость баллонов")
+    max_weight_of_transported_cylinders = models.IntegerField(null=True, blank=True,
+                                                              verbose_name="Максимальная масса перевозимых баллонов")
+    max_mass_of_transported_gas = models.IntegerField(null=True, blank=True,
+                                                      verbose_name="Максимальная масса перевозимого газа")
+    empty_weight = models.IntegerField(null=True, blank=True, verbose_name="Вес пустого т/с")
+    full_weight = models.IntegerField(null=True, blank=True, verbose_name="Вес полного т/с")
+    is_on_station = models.BooleanField(null=True, blank=True, verbose_name="Находится на станции")
+
+    def __str__(self):
+        return self.registration_number
+
+    class Meta:
+        verbose_name = "Прицеп"
+        verbose_name_plural = "Прицепы"
+
+
+class RailwayTanks(models.Model):
+    number = models.CharField(blank=False, max_length=10, verbose_name="Номер ж/д цистерны")
+    empty_weight = models.IntegerField(null=True, blank=True, verbose_name="Вес пустой цистерны")
+    full_weight = models.IntegerField(null=True, blank=True, verbose_name="Вес полной цистерны")
+    is_on_station = models.BooleanField(null=True, blank=True, verbose_name="Находится на станции")
+    entry_date = models.DateField(null=True, blank=True, verbose_name="Дата въезда")
+    entry_time = models.TimeField(null=True, blank=True, verbose_name="Время въезда")
+    departure_date = models.DateField(null=True, blank=True, verbose_name="Дата выезда")
+    departure_time = models.TimeField(null=True, blank=True, verbose_name="Время выезда")
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = "Ж/д цистерна"
+        verbose_name_plural = "Ж/д цистерны"
+
+
 class BalloonAmount(models.Model):
     reader_id = models.IntegerField(null=True, blank=True, verbose_name="Номер считывателя")
     amount_of_balloons = models.IntegerField(null=True, blank=True, verbose_name="Количество баллонов по датчику")
     change_date = models.DateField(null=True, blank=True, auto_now_add=True, verbose_name="Дата обновления")
     change_time = models.TimeField(null=True, blank=True, auto_now_add=True, verbose_name="Время обновления")
+
+
+class TTN(models.Model):
+    number = models.CharField(blank=False, max_length=20, verbose_name="Номер ТТН")
+    contract = models.CharField(blank=False, max_length=20, verbose_name="Номер договора")
+    name_of_supplier = models.CharField(blank=False, max_length=20, verbose_name="Поставщик")
+    gas_amount = models.FloatField(null=True, blank=True, verbose_name="Количество газа")
+    balloons_amount = models.FloatField(null=True, blank=True, verbose_name="Количество баллонов")
+    date = models.DateField(null=True, blank=True, verbose_name="Дата формирования накладной")
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = "Баллон"
+        verbose_name_plural = "Баллоны"
+
+
+class ShippingBatchBalloons(models.Model):
+    begin_date = models.DateField(null=True, blank=True, verbose_name="Дата начала приёмки")
+    begin_time = models.TimeField(null=True, blank=True, verbose_name="Время начала приёмки")
+    end_date = models.DateField(null=True, blank=True, verbose_name="Дата окончания приёмки")
+    end_time = models.TimeField(null=True, blank=True, verbose_name="Время окончания приёмки")
+    truck = models.ForeignKey(Truck, on_delete=models.DO_NOTHING, verbose_name="Автомобиль")
+    trailer = models.ForeignKey(Trailer, on_delete=models.DO_NOTHING, null=True, blank=True, default=0, verbose_name="Прицеп")
+    amount_of_rfid = models.IntegerField(null=True, blank=True, verbose_name="Количество баллонов по rfid")
+    amount_of_5_liters = models.IntegerField(null=True, blank=True, default=0, verbose_name="Количество 5л баллонов")
+    amount_of_20_liters = models.IntegerField(null=True, blank=True, default=0, verbose_name="Количество 20л баллонов")
+    amount_of_50_liters = models.IntegerField(null=True, blank=True, default=0, verbose_name="Количество 50л баллонов")
+    gas_amount = models.FloatField(null=True, blank=True, verbose_name="Количество принятого газа")
+    balloons_list = ArrayField(models.CharField(max_length=20), blank=True)
+    is_active = models.BooleanField(null=True, blank=True, verbose_name="В работе")
+    ttn = models.ForeignKey(TTN, on_delete=models.DO_NOTHING, default=0, verbose_name="ТТН")
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=0, verbose_name="Пользователь")
+
+    # def __str__(self):
+    #     return self.id
+
+    class Meta:
+        verbose_name = "Партия приёмки баллонов"
+        verbose_name_plural = "Партии приёмки баллонов"
+
+
+class ReceivingBatchBalloons(models.Model):
+    begin_date = models.DateField(null=True, blank=True, verbose_name="Дата начала отгрузки")
+    begin_time = models.TimeField(null=True, blank=True, verbose_name="Время начала отгрузки")
+    end_date = models.DateField(null=True, blank=True, verbose_name="Дата окончания отгрузки")
+    end_time = models.TimeField(null=True, blank=True, verbose_name="Время окончания отгрузки")
+    truck = models.ForeignKey(Truck, on_delete=models.DO_NOTHING, verbose_name="Автомобиль")
+    trailer = models.ForeignKey(Trailer, on_delete=models.DO_NOTHING, null=True, blank=True, default=0, verbose_name="Прицеп")
+    amount_of_rfid = models.IntegerField(null=True, blank=True, verbose_name="Количество баллонов по rfid")
+    amount_of_5_liters = models.IntegerField(null=True, blank=True, default=0, verbose_name="Количество 5л баллонов")
+    amount_of_20_liters = models.IntegerField(null=True, blank=True, default=0, verbose_name="Количество 20л баллонов")
+    amount_of_50_liters = models.IntegerField(null=True, blank=True, default=0, verbose_name="Количество 50л баллонов")
+    gas_amount = models.FloatField(null=True, blank=True, verbose_name="Количество отгруженного газа")
+    balloons_list = ArrayField(models.CharField(max_length=20), blank=True)
+    is_active = models.BooleanField(null=True, blank=True, verbose_name="В работе")
+    ttn = models.ForeignKey(TTN, on_delete=models.DO_NOTHING, default=0, verbose_name="ТТН")
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=0, verbose_name="Пользователь")
+
+    # def __str__(self):
+    #     return self.id
+
+    class Meta:
+        verbose_name = "Партия отгрузки баллонов"
+        verbose_name_plural = "Партии отгрузки баллонов"
+
+
+class ShippingBatchRailway(models.Model):
+    begin_date = models.DateField(null=True, blank=True, verbose_name="Дата начала приёмки")
+    begin_time = models.TimeField(null=True, blank=True, verbose_name="Время начала приёмки")
+    end_date = models.DateField(null=True, blank=True, verbose_name="Дата окончания приёмки")
+    end_time = models.TimeField(null=True, blank=True, verbose_name="Время окончания приёмки")
+    gas_amount = models.FloatField(null=True, blank=True, verbose_name="Количество принятого газа")
+    railway_tanks_list = ArrayField(models.CharField(max_length=20), blank=True, verbose_name="Список жд цистерн")
+    is_active = models.BooleanField(null=True, blank=True, verbose_name="В работе")
+    ttn = models.ForeignKey(TTN, on_delete=models.DO_NOTHING, default=0, verbose_name="ТТН")
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=0, verbose_name="Пользователь")
+
+    # def __str__(self):
+    #     return self.id
+
+    class Meta:
+        verbose_name = "Партия приёмки жд цистерн"
+        verbose_name_plural = "Партии приёмки жд цистерн"
