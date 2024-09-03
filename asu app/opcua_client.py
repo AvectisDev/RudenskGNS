9e20d38aec94c5134eefc5aaebb40c6c64ecedbc
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-09-04 00:40:10 +0300

    opc

--- asu app/opcua_client.py
+++ asu app/opcua_client.py
@@ -4,33 +4,15 @@ import schedule
 
 sys.path.insert(0, "..")
 RAILWAY = {
-'tank_weight': 0.0,
-'weight_is_stable': False,
+    'tank_weight': 0.0,
+    'weight_is_stable': False,
 }
 AUTO = {
-'weight': 0.0,
-'weight_is_stable': False,
-'mass_total': 0.0,
-'volume_total': 0.0
+    'weight': 0.0,
+    'weight_is_stable': False,
+    'mass_total': 0.0,
+    'volume_total': 0.0
 }
-
-def connect():
-    """
-    connect to OPC UA server
-    """
-
-    try:
-        client.connect()
-    except:
-        print("Dont connect to OPC UA serve")
-
-
-def disconnect():
-    """
-    Disconnect to OPC UA server
-    """
-
-    client.disconnect()
 
 
 def get_opc_value(addrstr):
@@ -47,22 +29,22 @@ def periodic_data():
     global RAILWAY, AUTO
 
     try:
-        connect()
-        print('Connent to OPC server succesfull')
+        client.connect()
+        print('Connect to OPC server successful')
 
         RAILWAY['tank_weight'] = get_opc_value("ns=4; s=Address Space.PLC_SU1.railway_tank_weight")
         RAILWAY['weight_is_stable'] = get_opc_value("ns=4; s=Address Space.PLC_SU1.railway_tank_weight_is_stable")
-        
+
         AUTO['weight'] = get_opc_value("ns=4; s=Address Space.PLC_SU2.auto_weight")
         AUTO['weight_is_stable'] = get_opc_value("ns=4; s=Address Space.PLC_SU2.weight_is_stable")
         AUTO['mass_total'] = get_opc_value("ns=4; s=Address Space.PLC_SU2.MicroMotion.Mass_total")
         AUTO['volume_total'] = get_opc_value("ns=4; s=Address Space.PLC_SU2.MicroMotion.Volume_total")
-        disconnect()
-        
-        print('Disconnent to OPC server')   
+
+        client.disconnect()
+        print('Disconnect from OPC server')
 
     except:
-        print('no connection to OPCUA server')
+        print('no connection to OPC server')
 
 
 schedule.every(5).seconds.do(periodic_data)
