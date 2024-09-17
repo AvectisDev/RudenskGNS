0a14696bf66288af2c3482bb4089166f07daabb0
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-09-17 22:08:47 +0300

    change video server times

--- asu app/setting.py
+++ asu app/setting.py
@@ -10,15 +10,15 @@ INTELLECT_URL = "http://10.10.0.252:10001/lprserver/GetProtocolNumbers"  # intel
 INTELLECT_SERVER_LIST = [
     {
         'id': '1',
-        'delta_minutes': 3
+        'delta_minutes': 2
     },
     {
         'id': '2,3',
-        'delta_minutes': 5
+        'delta_minutes': 10
     },
     {
         'id': '4,5',
-        'delta_minutes': 1200
+        'delta_minutes': 2
     }
 ]
 
