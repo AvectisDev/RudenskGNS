17c848f412e08a0462e367a5403110120c82a6e8
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-09-29 22:27:35 +0300

    auto-gas and railway batch fix

--- GNS/filling_station/api/api.py
+++ GNS/filling_station/api/api.py
@@ -366,7 +366,7 @@ class RailwayBatchView(APIView):
         batch_id = request.data.get('id')
         batch = get_object_or_404(RailwayBatch, id=batch_id)
 
-        if not request.data['is_active']:
+        if not request.data.get('is_active', True):
             current_date = datetime.now()
             request.data['end_date'] = current_date.date()
             request.data['end_time'] = current_date.time()
@@ -401,7 +401,7 @@ class AutoGasBatchView(APIView):
         batch_id = request.data.get('id')
         batch = get_object_or_404(AutoGasBatch, id=batch_id)
 
-        if not request.data.get('is_active', False):
+        if not request.data.get('is_active', True):
             current_date = datetime.now()
             request.data['end_date'] = current_date.date()
             request.data['end_time'] = current_date.time()
