d5cccdf373ff8fea79d050471d5631fb1fbfbea4
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-10-30 22:01:54 +0300

    work without docker

--- GNS/filling_station/management/commands/run_scripts.py
+++ GNS/filling_station/management/commands/run_scripts.py
@@ -2,9 +2,9 @@ from django.core.management.base import BaseCommand
 import subprocess
 
 
-# class Command(BaseCommand):
-#     help = 'Запуск скрипта main.py приложения обработки RFID считывателей'
-#
-#     def handle(self, *args, **kwargs):
-#         subprocess.Popen(['python', '../rfid app/main.py'])
-#         subprocess.Popen(['python', '../asu app/video_main.py'])
+class Command(BaseCommand):
+    help = 'Запуск скрипта main.py приложения обработки RFID считывателей'
+
+    def handle(self, *args, **kwargs):
+        subprocess.Popen(['python', '../rfid app/main.py'])
+        subprocess.Popen(['python', '../asu app/video_main.py'])
