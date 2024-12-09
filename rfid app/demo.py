166d7fe97ec92a69da3ab5155e4386caa420d4e1
KondratenkoAlexey <alexeykondratenko8@gmail.com>
2024-12-09 11:39:32 +0300

    update from Pinsk

--- rfid app/demo.py
+++ rfid app/demo.py
@@ -1,11 +1,3 @@
-# if reader['function'] is not None:  # если производится приёмка/отгрузка баллонов
-#     batch_data = await balloon_api.get_batch_balloons(reader['function'])
-#
-#     if batch_data:  # если партия активна - добавляем в неё пройденный баллон
-#         reader['batch']['batch_id'] = batch_data['id']
-#         reader['batch']['balloon_id'] = balloon_passport['id']
-#         await balloon_api.add_balloon_to_batch(reader)
-
 import time
 import db
 import aiohttp
